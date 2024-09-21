78b0ce63e0f938d8a1810ae2deecd32b8a06ccc8
npm-start-app <109803359+npm-start-app@users.noreply.github.com>
2024-09-21 15:54:46 +0200

    Update initController.js

--- controllers/initController.js
+++ controllers/initController.js
@@ -19,7 +19,7 @@ class Init {
 
     try {
       const __filename = fileURLToPath(import.meta.url);
-      const __dirname = dirname(__filename);
+      const __dirname = path.dirname(__filename);
 
       const data = await readFile(path.join(__dirname, '/../googleDrive_structure.json'), 'utf8');
 
